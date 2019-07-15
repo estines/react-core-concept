31721ca298190742fbed2088115c14af18272987
Dszix <dszix@Dszixs-MacBook-Pro.local>
2019-07-15 21:42:32 +0700

    init layout

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Layout, Menu, Input, List, Button } from 'antd';
+
+const { Header, Content } = Layout;
+const { Search } = Input;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <Layout className="layout" style={{ minHeight: '100vh' }}>
+      <Header>
+        <div className="logo" />
+        <Menu
+          theme="dark"
+          mode="horizontal"
+          defaultSelectedKeys={['2']}
+          style={{ lineHeight: '64px' }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <Menu.Item key="1">Todo List</Menu.Item>
+        </Menu>
+      </Header>
+      <Content style={{ background: '#fff', padding: '25px', margin: 50 }}>
+        <div style={{ padding: '24px 0' }}>
+          <Search
+            placeholder="input search text"
+            enterButton="Search"
+            size="large"
+            onSearch={value => console.log(value)}
+          />
+        </div>
+        <List
+          size="large"
+          bordered
+          dataSource={['test']}
+          renderItem={item => (
+            <List.Item>
+              <div style={{ display: 'flex', flex: 1 }}>
+                <div style={{ display: 'flex', flex: 1, alignItems: 'center' }}>
+                  {item}
+                </div>
+                <div
+                  style={{
+                    display: 'flex',
+                    justifyContent: 'flex-end'
+                  }}
+                >
+                  <Button type="primary" style={{ marginRight: 15 }}>
+                    Edit
+                  </Button>
+                  <Button type="danger">Delete</Button>
+                </div>
+              </div>
+            </List.Item>
+          )}
+        />
+      </Content>
+    </Layout>
   );
 }
 
